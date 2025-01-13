637363f053fcafb653faed24dff2fa76b0184549
HimaBindu Nara <naarahimabindu@gmail.com>
2025-01-13 21:37:14 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,22 @@
+package Empdetails;
+
+// class Object {
+//     Object() 
+//        {
+//        }
+//     }
+
+public class Student {      // public class Student extends Object{
+	
+	int roll;
+	String name;
+	
+	void study() {
+		System.out.println("Student is studying");
+	}
+	
+//	Student(){
+//		super();
+//	}
+
+}
